430a67888681a5c5ceedc27292a99c8f8034bea3
EliorUzan <68156663+EliorUzan@users.noreply.github.com>
2020-10-02 17:44:09 +0300

    some other function

---
+++ miscellaneous.js
@@ -0,0 +1,12 @@
+function refreshPage(){
+    window.location.reload();
+} 
+
+function backHome() {
+	window.location.href = "index.html";
+}
+
+function backToMentalMath(){
+    window.location.reload();
+    window.location.href = "#exercise_block";
+} 
